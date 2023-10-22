76d0c00ac81038ea572361aa9259700602d44ad5
shinchan <paryushidev0909@gmail.com>
2023-10-22 22:55:47 +0530

    vercel

--- server/index.js
+++ server/index.js
@@ -24,6 +24,8 @@ mongoose
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
+app.options("*", cors());
+
 const server = app.listen(process.env.PORT, () =>
   console.log(`Server started on ${process.env.PORT}`)
 );
