c8006c73410d551f21fe09e1ab5232e7e73d2fbe
shinchan <paryushidev0909@gmail.com>
2023-10-23 00:05:11 +0530

    vercel

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,7 +1,7 @@
 // export const host = "http://localhost:5000";
 export const host = "https://hun-han-server.vercel.app"; //connected to backend in vercel
 
-export const loginRoute = `${host}/api/auth/login`;
+export const loginRoute = `${host}/api/auth/login/`;
 export const registerRoute = `${host}/api/auth/register`;
 export const logoutRoute = `${host}/api/auth/logout`;
 export const allUsersRoute = `${host}/api/auth/allusers`;
